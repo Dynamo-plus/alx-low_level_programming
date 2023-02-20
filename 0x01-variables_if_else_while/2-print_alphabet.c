8f330ca117698932d0d211e1349c449b934469a4
dynamo-plus <abayomisundaygabriel@gmail.com>
2023-02-19 19:21:00 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return:  if zero (success)
+*/
+
+int main(void)
+{
+
+
+	char letter = "a";
+
+	while (letter <= "z")
+	{
+		++letter;
+
+		putchar(letter);
+	}
+
+	return (0);
+}
