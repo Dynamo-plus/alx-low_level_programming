632b9ab67bf29af4ededb4fb108d7f38a89a30af
dynamo-plus <abayomisundaygabriel@gmail.com>
2023-02-19 21:13:08 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return:  if zero (success)
+*/
+
+int main(void)
+{
+
+	int i = 0;
+
+	while (i < 10)
+	{
+		++i;
+		putchar(i);
+	}
+
+	putchar('\n');
+	return (0);
+}
