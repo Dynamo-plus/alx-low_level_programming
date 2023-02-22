30882778b9189bf5feb2ceb995f1fe1f0cee8a5f
dynamo-plus <abayomisundaygabriel@gmail.com>
2023-02-21 20:57:24 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lower case
+ * _putchar -  the function toprint the alphabets
+ */
+
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++
+	}
+
+	_putchar('\n');
+}
