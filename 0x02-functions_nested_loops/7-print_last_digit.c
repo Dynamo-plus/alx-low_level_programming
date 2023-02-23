85c88e497f215a4e6cd71c45ee0c6aaf201e9927
dynamo-plus <abayomisundaygabriel@gmail.com>
2023-02-22 17:47:46 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- *
+ * print_last_digit -  a fucntion to print the last digit of a number
+ * @n: parameter
+ * Return: last digit
 */
 
-int print_last_digit(int)
+int print_last_digit(int n)
 {
 	return (n % 10);
 }
