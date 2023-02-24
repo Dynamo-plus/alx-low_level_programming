336b4c240f787f51a6a4e512cb2eb82cf02e9510
dynamo-plus <abayomisundaygabriel@gmail.com>
2023-02-23 19:32:05 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplication function
+ * @a: first parameter
+ * @b: second parameter
+ * Return: the  product of the two params.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
