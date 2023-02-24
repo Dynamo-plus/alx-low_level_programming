9dbeb71e7437df2c89e2082239312b1cb7222657
dynamo-plus <abayomisundaygabriel@gmail.com>
2023-02-23 19:09:20 -0800

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * _isupper - function to check if input is in uppercase.
+ * _isdigit - function to check if input is in uppercase.
  * @c: parameter
  * Return: 1 if it is in upppercase,  0 if  otherwise.
  */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 0 && c <= 9)
 	{
 		return (1);
 	}
 	else
 	{
 		return (0);
+	}
 }
