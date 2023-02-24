5e6f1ee9384e74c2839ed05a9c47f39674663751
dynamo-plus <abayomisundaygabriel@gmail.com>
2023-02-23 19:50:35 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print number from 0 to 9
+ * Return: void;
+*/
+
+void print_numbers(void)
+{
+	int i;
+
+	i = '0';
+
+	while (i < '10')
+		_putchar(i);
+		i++;
+}
