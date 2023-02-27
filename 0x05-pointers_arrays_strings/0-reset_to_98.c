ff43540d5de9bb74c386a231d46d92a9017f4d99
dynamo-plus <abayomisundaygabriel@gmail.com>
2023-02-27 12:41:44 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - This will reset the value of n in main.c
+ *
+ *@*n - pointer as an argument of a function
+ *
+ * Return: return void
+*/
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
