03d8f75b35de474a630ba2202ac442f055e6ab44
dynamo-plus <abayomisundaygabriel@gmail.com>
2023-03-02 17:56:57 -0800

    Hot fx

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * leet - encode into 1337speak
- * @n: input value
- * Return: n value
+ * @mozart: input value
+ * Return: mozart value
  */
 char *leet(char *mozart)
 {
@@ -10,15 +10,15 @@ char *leet(char *mozart)
 	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
-	for (i = 0; n[i] != '\0'; i++)
+	for (i = 0; mozart[i] != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (n[i] == s1[j])
+			if (mozart[i] == s1[j])
 			{
-				n[i] = s2[j];
+				mozart[i] = s2[j];
 			}
 		}
 	}
-	return (n);
+	return (mozart);
 }
