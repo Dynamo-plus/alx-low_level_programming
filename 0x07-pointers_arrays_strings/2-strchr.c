73e555fa4ac393cb32d53e7483655563f341c812
dynamo-plus <abayomisundaygabriel@gmail.com>
2023-03-06 19:50:13 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_strchr - A function to find if a letter is in a string
+ * @s:= pointer
+ * @c: letter
+ *
+ * Return: 0(success)
+*/
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; i >= '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+	return (0);
+}
