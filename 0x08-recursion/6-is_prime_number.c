b86f80deae687e04833317855e942775a716b6dd
dynamo-plus <abayomisundaygabriel@gmail.com>
2023-03-07 18:00:01 -0800

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,24 @@
+#includes "main.h"
+
+/**
+ * is_prime_number - check if it is a prime number
+ *
+ * @n: inputed value
+ *
+ * Return: Always 0.
+ */
+
+int is_prime_number(int n)
+{
+	int i;
+
+	for (i = 2; i < n; i++)
+	{
+		if ((n % i) == 0)
+		{
+			return (0);
+		}
+	}
+
+	return (1);
+}
