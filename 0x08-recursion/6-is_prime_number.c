d37968358502fbba7999ffee7e1cfe6d28d0bf89
dynamo-plus <abayomisundaygabriel@gmail.com>
2023-03-07 19:33:08 -0800

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,30 +1,38 @@
 #include "main.h"
-
 /**
  * is_prime_number - check if it is a prime number
  *
  * @n: inputed value
- *
  * Return: Always 0.
  */
-
 int is_prime_number(int n)
 {
-	int i = 2;
-
-	if (n <= 2)
-	{
-		return ((n == 2) ? return (1) : return (0));
-	}
-
-	if ((n % i) == 0)
+	if (n <= 1)
 	{
 		return (0);
 	}
-	if ((i * i) > n)
+	return (real_prime(n, (n - 1)));
+}
+
+/**
+ * real_prime - check if it is a prime number
+ *
+ * @n: inputed value
+ * @i: parameter
+ * Return: Always 0.
+ */
+
+int real_prime(int n, int i)
+{
+	if (i == 1)
 	{
+
 		return (1);
 	}
+	if ((n % i) == 0 && i > 0)
+	{
+		return (0);
+	}
 
-	return (is_prime_number(n, i + 1));
+	return (real_prime(n, (i - 1)));
 }
