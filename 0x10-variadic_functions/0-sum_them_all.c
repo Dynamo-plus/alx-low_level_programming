1c3c8df7517e83b6d61b34a757f083dc8de566a0
dynamo-plus <abayomisundaygabriel@gmail.com>
2023-03-24 19:12:52 -0700

    Variadic function completed

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - A function to sum all the arguments up
+ * @n: length of argument passed;
+ * @...: infinity arguments
+ *
+ * Return: return sum if successful
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	int i;
+	int sum;
+
+	sum = 0;
+
+	va_list args;
+
+	va_start(args, n);
+
+	if (n == 0)
+		return (0);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+
+	return (sum);
+
+}
