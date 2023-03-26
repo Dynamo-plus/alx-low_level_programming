91b13434a76ddc926f13129e2fbbed4fd994d649
dynamo-plus <abayomisundaygabriel@gmail.com>
2023-03-25 23:55:46 -0700

    Structure updated - Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * init_dog - Initialize the properties of dog
+ * @d: container of the properties of dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+ *
+ * Return: Return the values of dog;
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+
+	if (d == NULL)
+		return (null);
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+	return (d);
+}
