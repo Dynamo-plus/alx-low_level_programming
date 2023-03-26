ad14f177cb47b05f8a0f99b52591ee00342f30a6
dynamo-plus <abayomisundaygabriel@gmail.com>
2023-03-25 23:39:11 -0700

    Structure updated - Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a collection of dog poperties
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the dog owner
+*/
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
